a3da3b45e76673ae8ea37af77cb801d3559f421d
Mohamad Ali Calanda <135398263+MohamadAli20@users.noreply.github.com>
2023-12-04 18:46:37 +0800

    commit

--- intermediate-algo-scripting.js
+++ intermediate-algo-scripting.js
@@ -108,6 +108,55 @@ function spinalCase(str) {
 }
 // console.log(spinalCase('This Is Spinal Tap')); // this-is-spinal-tap
 // console.log(spinalCase("The_Andy_Griffith_Show")); // the-andy-griffith-show
-console.log(spinalCase("AllThe-small Things"));
+// console.log(spinalCase("AllThe-small Things")); // all-the-small-things
+
+// DAY 3
+// Pig Latin
+function translatePigLatin(str) {
+    // match one or more consonant on the beginning of a string
+    const consonant = str.match(/^[bcdfghjklmnpqrstvwxyz]+/);
+    // match one vowel at any index of a string and one or more letters 
+    const vowel = str.match(/[aeiou][a-z]+/gi);
+    // if consonant is null or not found on a string on the beginning of a string
+    if(consonant === null){
+        str = vowel + "way";
+    }
+    // if vowel is not found at any index of a string
+    else if(vowel == null){
+        str = consonant + "ay";
+    }
+    // if both matched or found
+    else{
+        str = vowel + consonant +"ay";
+    }
+    return str;
+}
+   
+// console.log(translatePigLatin("consonant")); // onsonantcay
+// console.log(translatePigLatin("algorithm")) // algorithmway
+// console.log(translatePigLatin("rhythm")); // rhythmay
   
+// Search and Replace
+function myReplace(str, before, after) {
+    // check if first letter of before is uppercase
+    const isUpperCase = before.charAt(0) === before.charAt(0).toUpperCase();
+    // if isUpperCase is true
+    if(isUpperCase === true){
+        // convert first letter of after to uppercase
+        // concat to the rest of letters
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    }else{
+        // else if before first letter is lowercase
+        // convert first letter of after to lowercase as well
+        after = after.charAt(0).toLowerCase() + after.slice(1);
+    }
+    // replace before by after
+    str = str.replace(before, after);
+    return str;
+}
   
+// console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+// A quick brown fox leaped over the lazy dog
+
+// console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+// He is Sitting on the couch
