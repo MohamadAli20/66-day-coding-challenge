804ef8bf8fdf8dfa0037cda373ef7ab3df4f30a0
Mohamad Ali Calanda <135398263+MohamadAli20@users.noreply.github.com>
2023-12-14 19:17:25 +0800

    commit

--- js-certification.js
+++ js-certification.js
@@ -13,8 +13,76 @@ function palindrome(str) {
     
     return joinStr === reverseStr;
   }
-//   console.log(palindrome("1 eye for of 1 eye.")); // false
-console.log(palindrome("A man, a plan, a canal. Panama")); // true
+// console.log(palindrome("1 eye for of 1 eye.")); // false
+// console.log(palindrome("A man, a plan, a canal. Panama")); // true
   
-  
-  
+// Caesars Cipher
+function rot13(str) {
+  let arr = str.split('');
+  let cipher = {
+    'A':'N',
+    'B':'O',
+    'C':'P',
+    'D':'Q',
+    'E':'R',
+    'F':'S',
+    'G':'T',
+    'H':'U',
+    'I':'V',
+    'J':'W',
+    'K':'X',
+    'L':'Y',
+    'M':'Z'
+  }
+  // find the corresponding letter
+  for(let i in str){
+    for(let letter in cipher){
+      if(str[i] === letter){
+        arr[i] = cipher[letter];
+      }else if(str[i] === cipher[letter]){
+        arr[i] = letter;
+      }
+    }
+  }
+  // join
+  return arr.join("");
+}
+// console.log(rot13("SERR PBQR PNZC")); // FREE CODE CAMP
+// console.log(rot13("SERR CVMMN!")); //  FREE PIZZA!
+// console.log(rot13("SERR YBIR?")); // FREE LOVE?
+// console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+// Telephone checker
+// The following are examples of valid formats for US numbers 
+// (refer to the tests below for other variants):
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+// This is not finished yet, pattern should be fixed to return the correct boolean
+function telephoneCheck(str) {
+  let result;
+  if(str.length > 9){
+    // let regex = 
+    // result = /[\d-\s]*([(]+\d+[)]+|\d+)[-\s\d]+/.test(str);
+    result = str.match(/[1\s]*([(]+\d+[)]|-*\s*\d)+-*\s*\d+/g);
+    
+    if(result.join("") == str){
+      result = true;
+    }else{
+      result = false;
+    }
+  }else{
+    result = false;
+  }
+  return result; 
+}
+// console.log(telephoneCheck("555-555-5555")); // true
+// console.log(telephoneCheck("1 555-555-5555")); // true  
+// console.log(telephoneCheck("1 (555) 555-5555")); // true
+// console.log(telephoneCheck("5555555555")); // true
+// console.log(telephoneCheck("1 555)555-5555")); // false
+// console.log(telephoneCheck("555-5555")); // false
+// console.log(telephoneCheck("-1 (757) 622-7382"))
